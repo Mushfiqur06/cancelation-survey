1cbcc1dad1f59fa49a9d3814efffac027d825318
Mushfiqur Niloy <mushfiqur06@gmail.com>
2022-04-12 12:51:59 +0600

    Create Cancellation -survey page

---
+++ src/components/cancellation-survey/index.js
@@ -0,0 +1,263 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import BackArrowIcon from "../../icons/backArrowIcon";
+import BlackExclamationIcon from "../../icons/BlackExclamationIcon";
+import ExclamationIcon from "../../icons/ExclamationIcon";
+import LeftArrowIcon from "../../icons/LeftArrowIcon";
+import MultipleDropDown from "../common/MultipleDropDown";
+
+export default function CancellationSuvey() {
+  const navigate = useNavigate();
+  const [selectCancellationSurvey, setSelectCancellationSurvey] = useState("");
+  const [foundIssueTab, setFoundIssueTab] = useState("one-mejor-problem");
+  return (
+    <div className="cancellation-survey__body">
+      <div className="cancellation-survey__container">
+        <div className="cancellation-survey__header">
+          <div
+            onClick={() => navigate(-1)}
+            className="cancellation-survey__header__icon"
+          >
+            <BackArrowIcon />
+          </div>
+          <h3 className="cancellation-survey__header__title">
+            Cancellation Survey
+          </h3>
+        </div>
+        <div className="cancellation-survey">
+          <div className="cancellation-survey__sub-header">
+            <h2 className="cancellation-survey__sub-header__title">
+              We’re sad to see you go...{" "}
+            </h2>
+            <p className="cancellation-survey__sub-header__text">
+              Can you tell us why you want to cancel your membership?
+            </p>
+          </div>
+          <div className="cancellation__options">
+            <div
+              className="cancellation__options__info"
+              onClick={() =>
+                setSelectCancellationSurvey("recurring-membership")
+              }
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "recurring-membership"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                I didn’t realize it was a recurring membership
+              </div>
+            </div>
+            <div
+              className="cancellation__options__info"
+              onClick={() =>
+                setSelectCancellationSurvey("found-better-solution")
+              }
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "found-better-solution"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                Found a better solution
+              </div>
+              {selectCancellationSurvey === 'found-better-solution' && (
+                <div className="cancellation__options__info__content">
+                  <div className="issues__message">
+                    <div className="issues__message__textarea">
+                      <textarea placeholder="What is the better solution? If you don’t mind sharing. Your feedback is much appreaciated!" />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div
+              className="cancellation__options__info"
+              onClick={() => setSelectCancellationSurvey("too-expensive")}
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "too-expensive"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                It’s too expensive
+              </div>
+            </div>
+            <div
+              className="cancellation__options__info"
+              onClick={() =>
+                setSelectCancellationSurvey("not-working-properly")
+              }
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "not-working-properly"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                Bugs, things not working properly
+              </div>
+              {selectCancellationSurvey === "not-working-properly" && (
+                <div className="cancellation__options__info__content">
+                  <div className="dropdown">
+                    <div className="dropdown__label">
+                      <p>Which product(s) did you have an issue with?</p>{" "}
+                      <BlackExclamationIcon />
+                    </div>
+                    <MultipleDropDown />
+                  </div>
+                  <div className="tabs">
+                    <p>What was it?</p>
+                    <div className="tabs__list">
+                      <div
+                        className={`${
+                          foundIssueTab === "one-mejor-problem"
+                            ? "tabs__list__single active"
+                            : "tabs__list__single"
+                        }`}
+                        onClick={() => setFoundIssueTab("one-mejor-problem")}
+                      >
+                        One Major Problem
+                      </div>
+                      <div
+                        className={`${
+                          foundIssueTab === "various-things"
+                            ? "tabs__list__single active"
+                            : "tabs__list__single"
+                        }`}
+                        onClick={() => setFoundIssueTab("various-things")}
+                      >
+                        Various things
+                      </div>
+                    </div>
+                  </div>
+                  <div className="issues__message">
+                    <div className="issues__message__label">
+                      <p>What problem(s) did you encounter?</p>
+                      <BlackExclamationIcon />
+                    </div>
+                    <div className="issues__message__textarea">
+                      <textarea />
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div
+              className="cancellation__options__info"
+              onClick={() => setSelectCancellationSurvey("single-plugin")}
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "single-plugin"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                I just want to pay for a single plugin
+              </div>
+            </div>
+
+            <div
+              className="cancellation__options__info"
+              onClick={() => setSelectCancellationSurvey("not-using-wordpress")}
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "not-using-wordpress"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                Not using WordPress anymore
+              </div>
+            </div>
+
+            <div
+              className="cancellation__options__info"
+              onClick={() => setSelectCancellationSurvey("other")}
+            >
+              <div
+                className={`${
+                  selectCancellationSurvey === "other"
+                    ? "cancellation__options__info__radio active"
+                    : "cancellation__options__info__radio"
+                }`}
+              >
+                <input
+                  className="cancellation__options__info__input"
+                  type="radio"
+                  value="one"
+                  name="cancellation_survey"
+                />{" "}
+                Other
+              </div>
+            </div>
+          </div>
+          <div className="cancellation-survey__warning">
+            <div className="cancellation-survey__warning__icon">
+              <ExclamationIcon />
+            </div>
+            <div className="cancellation-survey__warning__text">
+              <p>
+                Are you absolutely sure you want to cancel? If you do you'll
+                lose access to so many cool things. And you'll lose the chance
+                to stay on this plan at the same cost ongoing (regardless of
+                future price increases).
+              </p>
+            </div>
+          </div>
+          <div className="cancellation-survey__btns">
+            <button className="btn" onClick={() => navigate(-1)}>
+              <LeftArrowIcon /> Back
+            </button>
+            <button className="btn btn-secondary">Cancel Membership</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
